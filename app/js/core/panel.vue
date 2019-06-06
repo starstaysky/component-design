c0bbe3713012b6f20ce94b6733139cfcfa57a38c
fuxing <fuxing@didichuxing.com>
2019-06-06 19:33:13 +0800

    基础组件设计part1

---
+++ app/js/core/panel.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="panel">
+        <h4></h4>
+        <div></div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+<style lang="scss">
+</style>
