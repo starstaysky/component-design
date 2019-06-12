413604b17cdc9aeb6148e17dba57e22312f9af87
fuxing <fuxing@didichuxing.com>
2019-06-12 20:58:01 +0800

    upate: 组件编写

--- app/js/core/panel.vue
+++ app/js/core/panel.vue
@@ -1,15 +1,35 @@
 <template>
-    <section class="panel">
-        <h4></h4>
-        <div></div>
+    <section :class="[pannelClass,cname]">
+        <h4>{{ title }}</h4>
+        <slot/>
     </section>
 </template>
 
 <script>
 export default {
+    props: {
+        cname: {
+            type: String,
+            default: "",
+        },
+        title: {
+            type: String,
+            default: "",
+        },
+    },
+
+    data() {
+        return {
+            pannelClass: "pannel",
+        }
+    },
 
 }
 
 </script>
 <style lang="scss">
+@import '../../css/element.scss';
+.pannel{
+   @include panel;
+}
 </style>
---
+++ app/js/core/slider.vue
@@ -0,0 +1,62 @@
+<!-- The ref attr used to find the swiper instance -->
+<template>
+    <swiper :options="swiperOption" ref="mySwiper">
+        <!-- slides -->
+        <swiper-slide v-for="item in items" :key="item.href">
+            <router-link :to="{name:item.href}">
+                <img :src="item.src"/>
+            </router-link>
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination" v-if="swiperOption.pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-scrollbar" slot="scrollbar"></div>
+    </swiper>
+</template>
+
+<script>
+
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+export default {
+    name: "Carrousel",
+    components: {
+        swiper,
+        swiperSlide,
+    },
+    data() {
+        return {
+            swiperOption: {
+                autoplay: true,
+                loop: true,
+                pagination: {
+                    el: ".swiper-pagination",
+                },
+                // some swiper options/callbacks
+                // 所有的参数同 swiper 官方 api 参数
+                // ...
+            },
+            items: {
+                type: Array,
+                default() {
+                    return [
+                        { },
+                    ]
+                },
+            },
+        }
+    },
+
+    computed: {
+        swiper() {
+            return this.$refs.mySwiper.swiper
+        },
+    },
+    mounted() {
+        this.swiper.slideTo(3, 1000, false)
+    },
+}
+</script>
+<style lang="css">
+@import '~swiper/dist/css/swiper.css'
+</style>
